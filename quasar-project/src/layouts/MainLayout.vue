8f584f1e182ce84e23d662923c8222f3e3d14c93
akozul <anakozul556@gmail.com>
2025-10-20 11:50:27 +0200

    Kreirane nove stranice

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App Knjiznica </q-toolbar-title>
+        <q-toolbar-title class="text-weight-thin text-h5"> Quasar App Knjiznica </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
@@ -76,6 +76,12 @@ const linksList = [
     caption: 'GitHub Knjiznica projekt',
     icon: 'favorite',
     link: 'https://github.com/akozul/Knjiznica.git',
+  },
+  {
+    title: 'Veleučilište u Rijeci',
+    caption: 'Stranica Veleučilišta u Rijeci',
+    icon: 'pageview',
+    link: 'https://veleri.hr',
   },
 ]
 
